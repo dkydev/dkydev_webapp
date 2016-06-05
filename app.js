2c6694ba6156d6db5ee73bfd14f09925dfe7767a
Nick Robson <n.robson91@gmail.com>
2016-06-04 22:06:36 -0400

    404 and 500 handling.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var port = 80;
 app.set("views", __dirname + "/views");
 app.engine('html', require('ejs').renderFile);
 app.set('view options', { layout: false });
@@ -10,12 +11,25 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/www'));
 app.get('/', function (req, res) {
     res.render("layout.html", {
-        "title": "dkydev.com",
+        "title": "dkydev",
         "template": "home.html"
     });
 });
-app.listen(8080, function () {
-    console.log("Webapp listening on port %d.", 8080);
+app.use(function (req, res, next) {
+    res.status(404).render("layout.html", {
+        "title": "404 - dkydev",
+        "template": "404.html"
+    });
+});
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).render("layout.html", {
+        "title": "500 - dkydev",
+        "template": "500.html"
+    });
+});
+app.listen(port, function () {
+    console.log("Webapp listening on port %d.", port);
 });
 exports.App = app;
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 
 var app = express();
+var port = 80;
 
 app.set("views", __dirname + "/views");
 app.engine('html', require('ejs').renderFile);
@@ -14,13 +15,28 @@ app.use(express.static(__dirname + '/www'));
 
 app.get('/', function (req, res) {
     res.render("layout.html", {
-        "title":"dkydev.com",
+        "title":"dkydev",
         "template": "home.html"
     });
 });
 
-app.listen(8080, function () {
-    console.log("Webapp listening on port %d.", 8080);
+app.use(function (req, res, next) {
+    res.status(404).render("layout.html", {
+        "title":"404 - dkydev",
+        "template" : "404.html"
+    });
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).render("layout.html", {
+        "title":"500 - dkydev",
+        "template" : "500.html"
+    });
+});
+
+app.listen(port, function () {
+    console.log("Webapp listening on port %d.", port);
 });
 
 export var App = app;
