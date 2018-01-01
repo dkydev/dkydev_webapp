201a8237ee78799936b7a9a43b53d92834f96be6
Nick <n.robson91@gmail.com>
2018-01-01 13:14:35 -0500

    Lets Encrypt express.

--- lib/app.ts
+++ lib/app.ts
@@ -5,9 +5,17 @@ import * as helmet from "helmet";
 import config from "../var/config";
 import router from "./router";
 import error from "./error";
-import {DKYSessionHandler} from "./session";
+import { DKYSessionHandler } from "./session";
 import * as session from "express-session";
-import {logger, errorLogger} from "./logger";
+import { logger, errorLogger } from "./logger";
+import * as le from "greenlock-express";
+import * as http from "http";
+import * as https from "https";
+import * as redirecthttps from "redirect-https";
+
+http.createServer(lex.middleware(redirecthttps())).listen(config.PORT, function () {
+    console.log("Listening for ACME http-01 challenges on", this.address());
+});
 
 var app: express.Application = express();
 
@@ -18,7 +26,7 @@ app.use(helmet());
 //app.use(DKYSessionHandler());
 
 app.use(session({
-    name : 'app.sid',
+    name: 'app.sid',
     secret: "1234567890QWERTY",
     resave: true,
     store: new session.MemoryStore(),
@@ -39,6 +47,49 @@ app.use(errorLogger());
 
 app.use(error());
 
-app.listen(config.PORT, () => {
-    console.log("Listening on port %d in %s mode.", config.PORT, process.env.NODE_ENV);
+https.createServer(lex.httpsOptions, lex.middleware(app)).listen(config.SSL_PORT, function () {
+    console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
 });
+
+
+var lex = le.create({
+    
+    server: config.LE_URL,
+    
+    approveDomains: (opts, certs, cb) => {
+        if (certs) {
+            opts.domains = ['dkydev.com', 'dkygames.com']
+        } else {
+            opts.email = 'nick@dkydev.com'; 
+            opts.agreeTos = true;
+        }
+        cb(null, { options: opts, certs: certs });
+    },
+    
+});
+
+var lex = require('greenlock-express').create({
+    server: 'production',
+    challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) },
+    store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' }),
+       approveDomains: function(opts, certs, cb) {
+         if(certs) {
+           opts.domains = ['domain1.com', 'domain2.com']
+         } else {
+           opts.email = 'youremail@example.com',
+           opts.agreeTos = true;
+         }
+         cb(null, { options: opts, certs: certs });
+       }
+       });
+       
+       //Optional: Add this line if you want to redirect all your traffic to https
+    require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
+      console.log("Listening for ACME http-01 challenges on", this.address());
+    });
+    
+    // handles your app 
+    var server = require('https').createServer(lex.httpsOptions, lex.middleware(app))
+    server.listen(443, function () {
+      console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+    });
--- var/config/index_development.ts
+++ var/config/index_development.ts
@@ -1,11 +1,13 @@
 export default class config {
-  public static HOST:string = "localhost";
-  public static PORT:number = 8080;
-  public static DATABASE_HOST:string = "";
-  public static DATABASE_PORT:number = 5432;
-  public static DATABASE_NAME:string = "";
-  public static DATABASE_USERNAME:string = "";
-  public static DATABASE_PASSWORD:string = "";
-  public static LOG_LEVEL:string = "";
-  public static SESSION_SECRET:string = "";
+  public static HOST: string = "localhost";
+  public static PORT: number = 80;
+  public static SSL_PORT: number = 443;
+  public static DATABASE_HOST: string = "";
+  public static DATABASE_PORT: number = 5432;
+  public static DATABASE_NAME: string = "";
+  public static DATABASE_USERNAME: string = "";
+  public static DATABASE_PASSWORD: string = "";
+  public static LOG_LEVEL: string = "";
+  public static SESSION_SECRET: string = "";
+  public static LE_URL: string = "staging";
 }
