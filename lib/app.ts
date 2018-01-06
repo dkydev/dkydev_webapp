88ee7881179630223b9f193ba75edf425b1a6e54
Nick Robson <n.robson91@gmail.com>
2018-01-06 15:13:26 -0500

    Reverted SSL handling, now handled via nginx.

--- lib/app.ts
+++ lib/app.ts
@@ -8,10 +8,6 @@ import error from "./error";
 import { DKYSessionHandler } from "./session";
 import * as session from "express-session";
 import { logger, errorLogger } from "./logger";
-import * as le from "greenlock-express";
-import * as http from "http";
-import * as https from "https";
-import * as redirecthttps from "redirect-https";
 
 var app: express.Application = express();
 
@@ -43,29 +39,6 @@ app.use(errorLogger());
 
 app.use(error());
 
-var lex = le.create({
-    
-    server: config.LE_URL,
-    
-    approveDomains: (opts, certs, cb) => {
-        if (certs) {
-            opts.domains = ['dkydev.com', 'images.dkydev.com', 'dkygames.com']
-        } else {
-            opts.email = 'nick@dkydev.com'; 
-            opts.agreeTos = true;
-        }
-        cb(null, { options: opts, certs: certs });
-    },
-
-    app: app
-    
-});
-
-require('http').createServer(lex.middleware(require('redirect-https')())).listen(config.PORT, function () {
-    console.log("Listening for ACME http-01 challenges on", this.address());
-});
-
-// handles your app
-require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(config.SSL_PORT, function () {
-    console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+app.listen(config.PORT, () => {
+    console.log("Listening on port %d in %s mode.", config.PORT, process.env.NODE_ENV);
 });
--- var/config/index_development.ts
+++ var/config/index_development.ts
@@ -1,7 +1,6 @@
 export default class config {
   public static HOST: string = "localhost";
   public static PORT: number = 80;
-  public static SSL_PORT: number = 443;
   public static DATABASE_HOST: string = "";
   public static DATABASE_PORT: number = 5432;
   public static DATABASE_NAME: string = "";
@@ -9,5 +8,4 @@ export default class config {
   public static DATABASE_PASSWORD: string = "";
   public static LOG_LEVEL: string = "";
   public static SESSION_SECRET: string = "";
-  public static LE_URL: string = "staging";
 }
