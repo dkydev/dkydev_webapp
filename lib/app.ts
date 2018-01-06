c5311cd4fcf91ced692e391dd8b714dca101d149
Nick Robson <n.robson91@gmail.com>
2018-01-06 00:05:15 -0500

    Fix LE.

--- lib/app.ts
+++ lib/app.ts
@@ -13,10 +13,6 @@ import * as http from "http";
 import * as https from "https";
 import * as redirecthttps from "redirect-https";
 
-http.createServer(lex.middleware(redirecthttps())).listen(config.PORT, function () {
-    console.log("Listening for ACME http-01 challenges on", this.address());
-});
-
 var app: express.Application = express();
 
 app.use(logger());
@@ -47,49 +43,31 @@ app.use(errorLogger());
 
 app.use(error());
 
-https.createServer(lex.httpsOptions, lex.middleware(app)).listen(config.SSL_PORT, function () {
-    console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
-});
-
-
 var lex = le.create({
     
     server: config.LE_URL,
     
     approveDomains: (opts, certs, cb) => {
         if (certs) {
-            opts.domains = ['dkydev.com', 'dkygames.com']
+            opts.domains = ['dkydev.com', 'images.dkydev.com', 'dkygames.com']
         } else {
             opts.email = 'nick@dkydev.com'; 
             opts.agreeTos = true;
         }
         cb(null, { options: opts, certs: certs });
     },
+
+    app: app,
+
+    debug: true
     
 });
 
-var lex = require('greenlock-express').create({
-    server: 'production',
-    challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) },
-    store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' }),
-       approveDomains: function(opts, certs, cb) {
-         if(certs) {
-           opts.domains = ['domain1.com', 'domain2.com']
-         } else {
-           opts.email = 'youremail@example.com',
-           opts.agreeTos = true;
-         }
-         cb(null, { options: opts, certs: certs });
-       }
-       });
-       
-       //Optional: Add this line if you want to redirect all your traffic to https
-    require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
-      console.log("Listening for ACME http-01 challenges on", this.address());
-    });
-    
-    // handles your app 
-    var server = require('https').createServer(lex.httpsOptions, lex.middleware(app))
-    server.listen(443, function () {
-      console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
-    });
+require('http').createServer(lex.middleware(require('redirect-https')())).listen(config.PORT, function () {
+    console.log("Listening for ACME http-01 challenges on", this.address());
+});
+
+// handles your app
+require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(config.SSL_PORT, function () {
+    console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+});
